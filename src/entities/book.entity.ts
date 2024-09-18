3c28b5d4c50e7b56846aee586e1923ea3a53cab3
michele-oliveira <micheleoliveiranovais24@gmail.com>
2024-09-18 19:48:10 -0300

    Criação do script seed e implementação do BooksService

---
+++ src/entities/book.entity.ts
@@ -0,0 +1,34 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({ name: "books" })
+export class Book {
+    @PrimaryGeneratedColumn("uuid")
+    id: string;
+
+    @Column()
+    image_1: string;
+
+    @Column()
+    image_2: string;
+
+    @Column()
+    book_name: string;
+
+    @Column()
+    author: string;
+
+    @Column()
+    publisher: string;
+
+    @Column()
+    language: string;
+
+    @Column()
+    pages: number;
+
+    @Column()
+    gender: string;
+
+    @Column()
+    description: string;
+}
---
+++ src/services/books/books.service.ts
@@ -0,0 +1,30 @@
+import { Repository } from "typeorm";
+import { Book } from "../../entities/book.entity";
+
+export class BooksService {
+  private booksRepository: Repository<Book>;
+
+  constructor(booksRepository: Repository<Book>) {
+    this.booksRepository = booksRepository;
+  }
+
+  async list() {
+    const books = await this.booksRepository.find();
+    books.forEach((book) => {
+      book.image_1 = `${process.env.APP_URL}/images/${book.image_1}`;
+      book.image_2 = `${process.env.APP_URL}/images/${book.image_2}`;
+    });
+
+    return books
+  }
+
+  async getById(bookId: string) {
+    const book = await this.booksRepository.findOneBy({ id: bookId });
+    if (book) {
+      book.image_1 = `${process.env.APP_URL}/images/${book.image_1}`;
+      book.image_2 = `${process.env.APP_URL}/images/${book.image_2}`;
+    }
+
+    return book;
+  }
+}
