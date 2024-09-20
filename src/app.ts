d397830b42055d0b6adb0e87d65bbeac92cc4349
michele-oliveira <micheleoliveiranovais24@gmail.com>
2024-09-20 19:52:40 -0300

    Criando função async bootstrap() para garantir que aplicação suba apenas quando o banco de dados conectar

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,36 @@
 import path from "path";
 import "reflect-metadata";
-import express from 'express';
+import express from "express";
 import dotenv from "dotenv";
 import { useExpressServer } from "routing-controllers";
 import { AppDataSource } from "./config/database/data-source";
-import { authorizationInterceptor, currentUserInterceptor } from "./config/http/interceptors";
+import {
+  authorizationInterceptor,
+  currentUserInterceptor,
+} from "./config/http/interceptors";
 
-const app = express();
-dotenv.config(); 
+async function bootstrap() {
+  const app = express();
+  dotenv.config();
 
-useExpressServer(app, {
+  useExpressServer(app, {
     cors: {
-        origin: "*"
+      origin: "*",
     },
     authorizationChecker: authorizationInterceptor,
     currentUserChecker: currentUserInterceptor,
-    controllers: [path.join(__dirname + '/controllers/**/*.controller.ts')]
-});
+    controllers: [path.join(__dirname + "/controllers/**/*.controller.ts")],
+  });
 
-app.use(express.static('public'));
-app.use('/images', express.static('images'));
+  app.use(express.static("public"));
+  app.use("/images", express.static("images"));
 
-AppDataSource.initialize();
+  await AppDataSource.initialize();
+  console.info("Database connected");
 
-app.listen(3333, () => {
+  app.listen(3333, () => {
     console.log("Server is running in port 3333");
-});
+  });
+}
+
+bootstrap();
