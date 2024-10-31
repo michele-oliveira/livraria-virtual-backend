c6b2d85ee9053c3844464a0ab48ec232d66a872d
michele-oliveira <micheleoliveiranovais24@gmail.com>
2024-10-31 19:44:56 -0300

    Adicionando 'Gender' e 'Subgender' ao seed

--- src/seed.ts
+++ src/seed.ts
@@ -1,8 +1,75 @@
 import { AppDataSource } from "./config/database/data-source"
 import { Book } from "./entities/book.entity"
+import { Gender } from "./entities/gender.entity";
+import { Subgender } from "./entities/subgender.entity";
+
+const createBookGendersAndSubgenders = async () => {
+    const gendersRepository = AppDataSource.getRepository(Gender);
+    const subgendersRepository = AppDataSource.getRepository(Subgender);
+
+    const actionGender = gendersRepository.create({ id: 1, name: "Ação" });
+    const scientificGender = gendersRepository.create({ id: 2, name: "Científicos" });
+    const economyGender = gendersRepository.create({ id: 3, name: "Economia" });
+    const scienceFictionGender = gendersRepository.create({ id: 4, name: "Ficção Científica" });
+    const religiousGender = gendersRepository.create({ id: 5, name: "Religiosos" });
+    const romanceGender = gendersRepository.create({ id: 6, name: "Romance" });    
+
+    await gendersRepository.save([
+        actionGender,
+        scientificGender,
+        economyGender,
+        scienceFictionGender,
+        religiousGender,
+        romanceGender
+    ]);
+
+    const actionSubgenders = subgendersRepository.create([
+        { id: "2f60417f-dcd3-4e17-9865-b8217aad8f60", name: "Aventura", gender: actionGender },
+        { id: "c6e1cfc0-3976-470f-ad57-cabcce75278f", name: "Espionagem", gender: actionGender },
+        { id: "c411ea74-abc4-4460-aa4f-9062d03421f7", name: "Policial", gender: actionGender },
+    ]);
+
+    const scientificSubgenders = subgendersRepository.create([
+        { id: "4e713d50-4ddb-4aeb-88af-57e99b47c4f7", name: "Astronomia", gender: scientificGender },
+        { id: "ec3c0f95-7224-417a-b0bf-76bdc85f8961", name: "Biologia", gender: scientificGender },
+        { id: "634a821e-3143-456d-a8cb-4453eef1a6b9", name: "Física", gender: scientificGender },
+    ]);
+
+    const economySubgenders = subgendersRepository.create([
+        { id: "61933d12-a78b-40ac-af27-dfae707c930d", name: "Microeconomia", gender: economyGender },
+        { id: "7881a972-8205-42ee-ad68-15f298478a96", name: "Macroeconomia", gender: economyGender },
+        { id: "68e1aaa7-e10f-4f6d-a8e1-b6df33b6a794", name: "Economia Internacional", gender: economyGender },
+    ]);
+
+    const scienceFictionSubgenders = subgendersRepository.create([
+        { id: "f1b2306a-2210-46b4-9d5b-c84b3d8377c4", name: "Cyberpunk", gender: scienceFictionGender },
+        { id: "57466e8a-ba4f-4010-8d93-c1ed8461c358", name: "Distopia", gender: scienceFictionGender },
+        { id: "4ef9281a-bc0b-49ed-ba44-568771d404a6", name: "Space Opera", gender: scienceFictionGender },
+    ]);
+
+    const religiousSubgenders = subgendersRepository.create([
+        { id: "7a5f1af6-7c14-41b2-8d3d-a53169ac5fc4", name: "Cristianismo", gender: religiousGender },
+        { id: "d8f714ba-c755-4746-9110-ee9ccf71f33a", name: "Espiritismo", gender: religiousGender },
+        { id: "19e54d3e-7d17-44bf-a7f9-540409297970", name: "Islamismo", gender: religiousGender },
+    ]);
+
+    const romanceSubgenders = subgendersRepository.create([
+        { id: "2f2d73c6-5994-4335-9ebe-619dec9047c4", name: "Contemporâneo", gender: scientificGender },
+        { id: "7af6cee6-4969-4b88-ad1f-8fb16e82bb2d", name: "Histórico", gender: scientificGender },
+        { id: "c6a185ee-a7a7-4cda-979a-28577108835b", name: "Policial", gender: scientificGender },
+    ]);
+
+    await subgendersRepository.save([
+        ...actionSubgenders,
+        ...scientificSubgenders,
+        ...economySubgenders,
+        ...scienceFictionSubgenders,
+        ...religiousSubgenders,
+        ...romanceSubgenders
+    ]);
+}
 
 const createBooks = async () => {
-    await AppDataSource.initialize();
     const booksRepository = AppDataSource.getRepository(Book);
 
     const newBook = booksRepository.create({
@@ -20,7 +87,13 @@ const createBooks = async () => {
     });
 
     await booksRepository.save(newBook);
+}
+
+const seed = async () => {
+    await AppDataSource.initialize();
+    await createBookGendersAndSubgenders();
+    await createBooks();
     await AppDataSource.destroy();
 }
 
-createBooks();
+seed();
