11945af77e4356f512d6c9f06e71aa65a72aebd1
michele-oliveira <micheleoliveiranovais24@gmail.com>
2024-10-31 17:10:06 -0300

    Melhorando indentação

--- src/entities/book.entity.ts
+++ src/entities/book.entity.ts
@@ -2,36 +2,36 @@ import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity({ name: "books" })
 export class Book {
-    @PrimaryGeneratedColumn("uuid")
-    id: string;
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
 
-    @Column()
-    image_1: string;
+  @Column()
+  image_1: string;
 
-    @Column()
-    image_2: string;
+  @Column()
+  image_2: string;
 
-    @Column()
-    book_name: string;
+  @Column()
+  book_name: string;
 
-    @Column()
-    author: string;
+  @Column()
+  author: string;
 
-    @Column()
-    publisher: string;
+  @Column()
+  publisher: string;
 
-    @Column()
-    language: string;
+  @Column()
+  language: string;
 
-    @Column()
-    pages: number;
+  @Column()
+  pages: number;
 
-    @Column()
-    gender: string;
+  @Column()
+  gender: string;
 
-    @Column()
-    description: string;
+  @Column()
+  description: string;
 
-    @Column()
-    book_file: string;
+  @Column()
+  book_file: string;
 }
